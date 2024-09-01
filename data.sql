fe99d54ff75718268bc4e5d2266b02571abfff2b
Carloooo <113890894+Carlo244@users.noreply.github.com>
2024-09-01 19:34:15 +0800

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,13 @@
+
 Select * from Users
 Where DateOfBirth between '2000-01-01' and '2004-12-31'
+
+SELECT * FROM Posts 
+WHERE Postedby IS 4
+
+SELECT GroupName
+FROM Groups
+
+SELECT * FROM GroupMembershipRequests
+WHERE GroupMemberUserID IS 2
 
