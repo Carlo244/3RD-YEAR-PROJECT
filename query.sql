acee1fe8d5bc80ea16e5e80429dd5838b448b0f6
Carloooo <113890894+Carlo244@users.noreply.github.com>
2024-09-02 20:21:53 +0800

    Update and rename data.sql to query.sql

---
+++ query.sql
@@ -0,0 +1,107 @@
+1
+SELECT 
+    UserID,
+    Username,
+    FirstName,
+    LastName,
+    DateOfBirth
+FROM 
+    Users
+WHERE 
+    DateOfBirth BETWEEN '2000-01-01' AND '2004-12-31';
+
+
+2
+SELECT 
+    PostID,
+    PostDescription,
+    IsPublic,
+    IsOnlyForFriends,
+    GroupID,
+    DatePosted
+FROM 
+    Posts
+WHERE 
+    PostedBy = 4;
+
+3
+SELECT 
+    GroupID,
+    GroupName
+FROM 
+    Groups;
+
+
+4
+SELECT 
+    g.GroupID,
+    g.GroupName,
+    gmr.GroupMemberShipRequestsID,
+    gmr.IsGroupMemberShipAccepted,
+    gmr.DateAccepted
+FROM 
+    GroupMembershipRequests gmr
+JOIN 
+    Groups g 
+    ON gmr.GroupID = g.GroupID
+WHERE 
+    gmr.GroupMemberUserID = 2;
+
+SELECT * FROM GroupMembershipRequests
+WHERE GroupMemberUserID IS 2
+
+    
+5
+SELECT 
+    u.UserID,
+    u.FirstName,
+    u.LastName
+FROM 
+    Friends as f
+JOIN 
+    Users as u 
+    ON (f.FriendWhoAdded = u.UserID AND f.FriendBeingAdded = 2)
+    OR (f.FriendBeingAdded = u.UserID AND f.FriendWhoAdded = 2)
+WHERE 
+    f.IsAccepted = TRUE;
+
+
+6
+SELECT 
+	FriendWhoAdded,
+    FriendBeingAdded,
+    IsAccepted,
+    DateAdded
+FROM Friends 
+
+WHERE FriendWhoAdded = 1;
+
+
+7
+SELECT 
+	PostID,
+    PostDescription,
+    PostedBy,
+    IsPublic,
+    IsOnlyForFriends,
+    GroupID,
+    DatePosted
+FROM 
+	Posts
+WHERE 
+GroupID = 2
+    AND IsOnlyForFriends = TRUE;
+
+
+
+8
+SELECT 
+    GroupMemberShipRequestsID,
+    GroupMemberUserID,
+    DateAccepted
+FROM 
+    GroupMembershipRequests
+WHERE 
+    GroupID = 2
+    AND IsGroupMemberShipAccepted = FALSE;
+
